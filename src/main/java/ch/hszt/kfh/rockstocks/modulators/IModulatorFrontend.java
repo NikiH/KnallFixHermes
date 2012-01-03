ef08023dd7300b6ac5395424cad257a2bea138e0
niki <niki@debianMdP-niki.zhaw.ch>
2012-01-03 17:24:32 +0100

    Interfaces für modulare GUI-Komponenten der modulators

--- src/main/java/ch/hszt/kfh/rockstocks/modulators/IModulatorFrontend.java
+++ src/main/java/ch/hszt/kfh/rockstocks/modulators/IModulatorFrontend.java
@@ -1,5 +1,11 @@
 package ch.hszt.kfh.rockstocks.modulators;
 
+import javax.swing.JComponent;
+
 public interface IModulatorFrontend {
+	
+	public boolean isActive();
+	
+	public JComponent getComponent();
 
 }
---
+++ src/main/java/ch/hszt/kfh/rockstocks/modulators/PitchShiftModulatorFrontend.java
@@ -0,0 +1,19 @@
+package ch.hszt.kfh.rockstocks.modulators;
+
+import javax.swing.JComponent;
+
+public class PitchShiftModulatorFrontend implements IModulatorFrontend {
+
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public JComponent getComponent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
