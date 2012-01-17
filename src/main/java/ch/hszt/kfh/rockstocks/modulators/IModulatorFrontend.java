d6bf9bfeb5a7d0f6aa1e708707cec848d2bfccae
NikiH <hausanik@students.zhaw.ch>
2012-01-17 11:17:58 +0100

    new methods frontend

--- src/main/java/ch/hszt/kfh/rockstocks/modulators/IModulatorFrontend.java
+++ src/main/java/ch/hszt/kfh/rockstocks/modulators/IModulatorFrontend.java
@@ -2,7 +2,13 @@ package ch.hszt.kfh.rockstocks.modulators;
 
 import javax.swing.JComponent;
 
+import ch.hszt.kfh.rockstocks.series.TimeSeriesProviderRegistry;
+
 public interface IModulatorFrontend {
+	
+	public void setRegistry(TimeSeriesProviderRegistry registry);
+	
+	public Object getSelectedItem();
 	
 	public boolean isActive();
 	
--- src/main/java/ch/hszt/kfh/rockstocks/modulators/PitchShiftModulatorFrontend.java
+++ src/main/java/ch/hszt/kfh/rockstocks/modulators/PitchShiftModulatorFrontend.java
@@ -2,6 +2,8 @@ package ch.hszt.kfh.rockstocks.modulators;
 
 import javax.swing.JComponent;
 
+import ch.hszt.kfh.rockstocks.series.TimeSeriesProviderRegistry;
+
 public class PitchShiftModulatorFrontend implements IModulatorFrontend {
 
 	@Override
@@ -12,6 +14,18 @@ public class PitchShiftModulatorFrontend implements IModulatorFrontend {
 
 	@Override
 	public JComponent getComponent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRegistry(TimeSeriesProviderRegistry registry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getSelectedItem() {
 		// TODO Auto-generated method stub
 		return null;
 	}
