1e62024f61e4656416602fa8e1ce132bb5f44f4a
NikiH <hausanik@students.zhaw.ch>
2012-01-17 11:28:42 +0100

    new methods frontend

--- src/main/java/ch/hszt/kfh/rockstocks/modulators/IModulatorFrontend.java
+++ src/main/java/ch/hszt/kfh/rockstocks/modulators/IModulatorFrontend.java
@@ -1,17 +1,45 @@
 package ch.hszt.kfh.rockstocks.modulators;
 
 import javax.swing.JComponent;
+import javax.tools.JavaCompiler;
 
 import ch.hszt.kfh.rockstocks.series.TimeSeriesProviderRegistry;
 
+/**
+ * Interface für Frontends der einzelnen Modulatoren.
+ * 
+ * @author The Team
+ * 
+ */
+
 public interface IModulatorFrontend {
 	
+	/**
+	 * Setzt die registrierten Provider für die grafische Auswahl.
+	 * @param TimeSeriesProviderRegistry
+	 *
+	 */	
 	public void setRegistry(TimeSeriesProviderRegistry registry);
 	
+	/**
+	 * Gibt die aktuelle Auswahl zurück.
+	 * 
+	 * @return Object
+	 */	
 	public Object getSelectedItem();
 	
+	/**
+	 * Gibt zurück, ob die Checkbox aktiviert wurde.
+	 * 
+	 * @return boolean
+	 */	
 	public boolean isActive();
 	
+	/**
+	 * Gibt das ganze Frontend zurück.
+	 * 
+	 * @return JComponent
+	 */	
 	public JComponent getComponent();
 
 }
